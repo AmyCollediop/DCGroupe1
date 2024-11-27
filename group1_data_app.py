4c7919de954b59cd6425b65e27e0035119fad847
AmyCollediop <amy.c.diop@aims-senegal.org>
2024-11-27 23:23:20 +0100

    Add files via upload

--- group1_data_app.py
+++ group1_data_app.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup as bs
 from requests import get
 import streamlit as st
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit.components.v1 as components
 
 st.markdown("<h1 style='text-align: center; color: black;'>GROUP 1 DATA APP</h1>", unsafe_allow_html=True)
 
@@ -38,13 +40,24 @@ def scrape_all_bs(pages_nb, link):
     data = []
     
     for page in range(1, pages_nb + 1):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Accept-Language': 'en-US,en;q=0.9',
+        }
+        
         url = f"{link}?page={page}"
-        res = get(url)
+        res = get(url, headers=headers)
+        
+        if res.status_code != 200:
+            st.write(f"Erreur sur la page {page}: Code HTTP {res.status_code}")
+            continue
         
         soup = bs(res.text, 'html.parser')
             
         # Find all containers for listings
         containers = soup.find_all("div", class_="listing-card__content")
+
+        if not containers:
+            st.write(f"Aucun conteneur trouvé sur la page {page}")
             
         for container in containers:
             try:
@@ -77,20 +90,79 @@ def scrape_all_bs(pages_nb, link):
                 
             except Exception as e:
                 pass  # Skip this container in case of an error
-    
+        #time.sleep(2)  # Wait 2 seconds before the next request
     # Convert collected data to a DataFrame
     df = pd.DataFrame(data)
     return df
 
-if st.button("Computer data"):
-    if options=="Scrape data using BeautifulSoup":
+# Function for loading the data
+def load_(dataframe, title, key) :
+    st.markdown("""
+    <style>
+    div.stButton {text-align:center}
+    </style>""", unsafe_allow_html=True)
+
+    if st.button(title,key):
+      
+        st.subheader('Display data dimension')
+        st.write('Data dimension: ' + str(dataframe.shape[0]) + ' rows and ' + str(dataframe.shape[1]) + ' columns.')
+        st.dataframe(dataframe)
+
+#The conditions of the options of the sidebar
+if options=="Scrape data using BeautifulSoup":
+    if st.button("Computer data"):
         df = scrape_all_bs(pages_indexes, url_ordis)
-        
+        st.subheader('Display data dimension')
+        st.write('Data dimension: ' + str(df.shape[0]) + ' rows and ' + str(df.shape[1]) + ' columns.')
+        st.dataframe(df)
+    
+    elif st.button("Telephones data"):
+        df = scrape_all_bs(pages_indexes, url_phones)
+        st.subheader('Display data dimension')
+        st.write('Data dimension: ' + str(df.shape[0]) + ' rows and ' + str(df.shape[1]) + ' columns.')
+        st.dataframe(df)
+    
+    elif st.button("Cinema data"):
+        df = scrape_all_bs(pages_indexes, url_cinema)
         st.subheader('Display data dimension')
         st.write('Data dimension: ' + str(df.shape[0]) + ' rows and ' + str(df.shape[1]) + ' columns.')
         st.dataframe(df)
 
 
+# load the data
+elif options == "Download scraped data":
+    load_(pd.read_csv('data/ordi_expat_dakar.csv'), 'Computers data', '1')
+    load_(pd.read_csv('data/phone_expat_dakar.csv'), 'Telephones data', '2')
+    load_(pd.read_csv('data/expat_cinema_dakar.csv'), 'Cinema data', '3')
+   
+elif options == "Dashboard of the data":
+    df_ordis = pd.read_csv('data/ordi_expat_dakar.csv')
+    df_phones = pd.read_csv('data/phone_expat_dakar.csv')
+    df_cinema= pd.read_csv('data/expat_cinema_dakar.csv')
+
+    df_ordis = df_ordis.drop(['web-scraper-order','web-scraper-start-url'], axis = 1) #We drop the useless columns
+    df_ordis['price'] = pd.to_numeric(df_ordis['price'].str.replace('F Cfa', '', regex=False).str.replace(' ', ''),  errors='coerce')#We clean the 'price' column
+
+    avg_prices_by_brand = df_ordis.groupby('brand')['price'].mean()
+
+    plot1 = plt.figure(figsize=(11, 7))
+    avg_prices_by_brand.plot(kind='bar', color=(0.2, 0.4, 0.2, 0.6))
+    plt.title('Average price per brand', fontsize=14)
+    plt.xlabel('Brands', fontsize=12)
+    plt.ylabel('Average prices (FCFA)', fontsize=12)
+    plt.xticks(rotation=45, ha='right')#Rotate the names of the bars
+    st.pyplot(plot1)
+
+else:
+    components.html("""
+    <iframe src="https://ee.kobotoolbox.org/i/QSMp2YGW" width="800" height="600"></iframe>
+    """,height=1100,width=800)
+
+    
+    
+
+    
+    
 
 
 
